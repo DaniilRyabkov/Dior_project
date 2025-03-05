5206ee02659fdae2ff34404cc355dc1560e23931
Daniil <skylaser33@gmail.com>
2025-03-05 20:47:58 +0300

    new

--- js/main.js
+++ js/main.js
@@ -21,4 +21,7 @@ const swiperTop = new Swiper('.top-swiper',{
       prevEl: ".swiper-button-prev",
     },
   });
-  
+  document.querySelectorAll('.accordeon__triger').forEach((item) => { item.addEventListener('click', () => {
+  item.parentNode.classList.toggle('accordeon__item--active') 
+  })
+});
