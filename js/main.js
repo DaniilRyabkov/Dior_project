c6c8775ed6baa78d13284f6c44e0d588330e8215
Ryabkov Daniil <ryabkovdaniil@MacBook-Air-Ryabkov.local>
2025-06-14 11:24:22 +0300

    new code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 const swiperTop = new Swiper('.top-swiper',{
-    // Optional parameters
     effect: "fade",
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    // Navigation arrows
+    //autoplay: {
+     // delay: 2500,
+      //disableOnInteraction: false, },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -22,6 +19,6 @@ const swiperTop = new Swiper('.top-swiper',{
     },
   });
   document.querySelectorAll('.accordeon__triger').forEach((item) => { item.addEventListener('click', () => {
-  item.parentNode.classList.toggle('accordeon__item--active') 
+    item.parentNode.classList.toggle('accordeon__item--active') 
   })
 });
